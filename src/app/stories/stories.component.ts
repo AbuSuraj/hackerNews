0fd74a7bf489d2a53718a23617ade99f8c55141c
Abu Suraj <abusurajbd@gmail.com>
2024-02-14 23:46:39 +0600

    implement basic UI & map data  of stories

---
+++ src/app/stories/stories.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { TopStoriesService } from '../service/top-stories.service';
+
+@Component({
+  selector: 'app-stories',
+  templateUrl: './stories.component.html',
+  styleUrls: ['./stories.component.scss']
+})
+export class StoriesComponent implements OnInit {
+  topStories: any[] = [];
+
+  constructor(private topStoriesService: TopStoriesService) { }
+
+  ngOnInit(): void {
+    this.getTopStories();
+  }
+
+  getTopStories(){
+    this.topStoriesService.getTopStories().subscribe(storyIds => {
+      this.topStories = [];
+      storyIds.slice(0, 10).forEach(storyId => {
+        this.topStoriesService.getStoryDetails(storyId).subscribe(story => {
+          this.topStories.push(story);
+        });
+      });
+      console.log(this.topStories);
+      
+    });
+
+
+  }
+}
